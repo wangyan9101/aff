ecbd5add6940ecb80a4462ebce3a58fc06fee47e
reus <reusee@gmail.com>
2016-12-12 14:11:28 +0800

    fix copy_update; remove in-place update function

--- __tests__/update.test.js
+++ __tests__/update.test.js
@@ -1,134 +1,6 @@
 import {update, $inc, $dec, $any, $merge, make_updater, copy_update} from '../state'
 import {object_has_tag} from '../object'
 
-test('update $inc', () => {
-  let res;
-
-  res = update({
-    a: 3,
-  }, 'a', $inc);
-  expect(res.a).toBe(4);
-
-  res = update({
-    a: {
-      b: 9,
-    },
-  }, 'a', 'b', $inc);
-  expect(res.a.b).toBe(10);
-
-  res = update({
-    a: {
-      b: {
-        c: 1,
-      },
-    },
-  }, 'a', 'b', 'c', $inc);
-  expect(res.a.b.c).toBe(2);
-});
-
-test('update $dec', () => {
-  let res;
-
-  res = update({
-    a: 3,
-  }, 'a', $dec);
-  expect(res.a).toBe(2);
-
-  res = update({
-    a: {
-      b: 9,
-    },
-  }, 'a', 'b', $dec);
-  expect(res.a.b).toBe(8);
-
-  res = update({
-    a: {
-      b: {
-        c: 1,
-      },
-    },
-  }, 'a', 'b', 'c', $dec);
-  expect(res.a.b.c).toBe(0);
-});
-
-test('update $any', () => {
-  let res = update({
-    a: 1,
-    b: 2,
-    c: 3,
-    d: 4,
-  }, $any, $inc);
-  expect(res.a).toBe(2);
-  expect(res.b).toBe(3);
-  expect(res.c).toBe(4);
-  expect(res.d).toBe(5);
-});
-
-test('update set', () => {
-  let res;
-
-  res = update({
-    a: {
-      b: {
-        c: {
-          d: 5,
-        },
-      },
-    },
-  }, 'a', 'b', 'c', 'd', 20);
-  expect(res.a.b.c.d).toBe(20);
-});
-
-test('update any path', () => {
-  let res = update({
-    a: {
-      foo: 1,
-    },
-    b: {
-      foo: 2,
-    },
-    c: {
-      foo: 3,
-    },
-  }, $any, 'foo', $inc);
-  expect(res.a.foo).toBe(2);
-  expect(res.b.foo).toBe(3);
-  expect(res.c.foo).toBe(4);
-});
-
-test('update any path 2', () => {
-  let res = update({
-    a: {
-      foo: 1,
-    },
-    b: {
-      bar: 2,
-    },
-    c: {
-      baz: 3,
-    },
-  }, $any, $any, $inc);
-  expect(res.a.foo).toBe(2);
-  expect(res.b.bar).toBe(3);
-  expect(res.c.baz).toBe(4);
-});
-
-test('updater', () => {
-  let state = {
-    foo: 42,
-  };
-  let before;
-  let after;
-  let update = make_updater(state, () => {
-    before = 1;
-  }, () => {
-    after = 2;
-  });
-  update('foo', 42);
-  expect(before).toBe(1);
-  expect(after).toBe(2);
-});
-
 test('copy update', () => {
   expect(copy_update(1)).toBe(1);
   expect(copy_update(true)).toBe(true);
@@ -263,4 +135,12 @@ test('copy update with getter', () => {
   expect(new_obj.foo).toBe(84);
   expect(obj.bar).toBe(99);
   expect(obj.foo).toBe(198); // should not change
+});
+
+test('copy update extra path', () => {
+  let obj = {};
+  let new_obj = copy_update(obj, 'foo', $any, 42);
+  expect(new_obj.foo !== undefined).toBe(true);
+  expect(typeof new_obj.foo === 'object').toBe(true);
+  expect(Object.keys(new_obj.foo).length).toBe(0);
 });
--- state.js
+++ state.js
@@ -10,72 +10,6 @@ export let $merge = (obj) => ({
 });
 export let $any = { __predict_any: true };
 
-// update
-
-export function update(obj, ...args) {
-  if (args.length == 2) { 
-    // the last path node
-    let [path, op] = args;
-    if (path === $any) {
-      // match all path nodes
-      for (let key in obj) {
-        apply_op(obj, key, op);
-      }
-    } else {
-      // match one path node
-      apply_op(obj, path, op);
-    }
-  } else if (args.length > 2) { 
-    // path finding
-    let path = args[0];
-    if (path === $any) {
-      for (let key in obj) {
-        update(obj[key], ...args.slice(1));
-      }
-    } else {
-      update(obj[path], ...args.slice(1));
-    }
-  } else {
-    throw['bad path and op to update()', args];
-  }
-  return obj
-}
-
-function apply_op(obj, key, op) {
-  if (op === $inc) {
-    // self increment
-    obj[key]++;
-  } else if (op === $dec) {
-    // self decrement
-    obj[key]--;
-  } else {
-    // default to set
-    obj[key] = op;
-  }
-}
-
-export function make_updater(obj, after, before) {
-  return function(...args) {
-    before && before();
-    update(obj, ...args);
-    after && after();
-  }
-}
-
-export function make_debug_updater(obj, after, before) {
-  return make_updater(obj, () => {
-    console.log('%c AFTER  %o', 'background: #AAA; color: white',
-      JSON.parse(JSON.stringify(state)));
-    after && after();
-  }, () => {
-    before && before();
-    console.log('%c BEFORE %o', 'background: #666; color: white', 
-      JSON.parse(JSON.stringify(state)));
-    console.log('%c CHANGE %o', 'background: #888; color: white',
-      args);
-  });
-}
-
 // path-copying update
 
 export function copy_update(obj, ...args) {
@@ -156,7 +90,7 @@ export function copy_update(obj, ...args) {
           }
         }
         if (!key_updated) { // insert
-          new_obj[key] = copy_update(undefined, args[1]);
+          new_obj[key] = copy_update(undefined, ...args.slice(1));
           if (typeof args[1] === 'object' && !object_has_tag(args[1], 'frozen')) {
             all_frozen = false;
           }
