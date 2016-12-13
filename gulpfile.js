dbd0cea5a3bf59d153b48d8e1f11ef85ae795a0a
reus <reusee@gmail.com>
2016-12-13 14:31:36 +0800

    add lib dir

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,26 @@ var gulp = require('gulp');
 var webpack = require('gulp-webpack');
 
 gulp.task('default', function() {
+  gulp.src('index.js')
+    .pipe(webpack({
+      output: {
+        filename: 'affjs.js',
+        library: 'affjs',
+        libraryTarget: 'umd',
+        umdNamedDefine: true,
+      },
+      devtool: 'source-map',
+      module: {
+        loaders: [
+          {
+            test: /\.js$/,
+            exclude: /node_modules/,
+            loader: 'babel-loader',
+          },
+        ],
+      },
+    }))
+  .pipe(gulp.dest('lib'));
 });
 
 [
--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 import * as dom from './dom'
 import * as state from './state'
 import * as tags from './tags'
+import * as app from './app'
 
 module.exports = {
   dom: dom,
   state: state,
   tags: tags,
+  app: app,
 };
