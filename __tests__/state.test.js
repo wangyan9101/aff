391668c2b721129c8786148b6b6ec51dabefc97c
reus <reusee@gmail.com>
2017-01-01 19:44:04 +0800

    remove function `pick`

--- __tests__/state.test.js
+++ __tests__/state.test.js
@@ -1,26 +1,4 @@
-import { pick, State } from '../index'
-
-test('pick', () => {
-  expect(pick([], 'foo')).toEqual([]);
-  expect(pick([1], 'foo')).toEqual([]);
-  expect(pick([1, 2], 'foo')).toEqual([]);
-  expect(pick([], 0)).toEqual([]);
-  expect(pick([1], 0)).toEqual([1]);
-  expect(pick([1, 2, 3], 0, 2)).toEqual([1, 3]);
-  expect(pick({}, 'foo')).toEqual({});
-  expect(pick({}, 'foo', 'bar')).toEqual({});
-  expect(pick({baz: false}, 'foo', 'bar')).toEqual({});
-  expect(pick({foo: false}, 'foo', 'bar')).toEqual({foo: false});
-  expect(pick({foo: false, bar: 'bar'}, 'foo', 'bar')).toEqual({foo: false, bar: 'bar'});
-  expect(pick({foo: false, bar: 'bar'}, 'bar')).toEqual({bar: 'bar'});
-  expect(pick({foo: false, bar: 'bar'})).toEqual({});
-});
-
-test('pick false', () => {
-  expect(() => {
-    pick(false, 'foo');
-  }).toThrowError('not pickable');
-});
+import { State } from '../index'
 
 test('cover', () => {
   let state = new State();
--- index.js
+++ index.js
@@ -7,7 +7,6 @@ export * from './operations';
 export * from './tags';
 export * from './tagged';
 export * from './event';
-export * from './utils';
 export * from './mutable_state';
 export * from './state';
 export * from './bind';
