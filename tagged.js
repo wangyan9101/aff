755916b78146e3cc829e1fc10a167f3d9c6c76e6
reus <reusee@gmail.com>
2017-01-03 14:46:23 +0800

    clean up template tagging function codes

--- tagged.js
+++ tagged.js
@@ -2,29 +2,23 @@ export function Selector(str) {
   this.str = str;
 }
 
-export function $(strings, ...values) {
-  let str = '';
-  for (let i = 0; i < strings.length; i++) {
-    str += strings[i];
-    if (values[i] !== undefined) {
-      str += values[i];
-    }
-  }
-  return new Selector(str);
-}
-
 export function Css(str) {
   this.str = str;
 }
 
-export function css(strings, ...values) {
-  let str = '';
-  for (let i = 0; i < strings.length; i++) {
-    str += strings[i];
-    if (values[i] !== undefined) {
-      str += values[i];
+function makeTagger(constructor) {
+  function tag(strings, ...values) {
+    let str = '';
+    for (let i = 0; i < strings.length; i++) {
+      str += strings[i];
+      if (values[i] !== undefined) {
+        str += values[i];
+      }
     }
+    return new constructor(str);
   }
-  return new Css(str);
+  return tag;
 }
 
+export const $ = makeTagger(Selector);
+export const css = makeTagger(Css);
