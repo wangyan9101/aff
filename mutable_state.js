81d449bb719a2bd96906314cb9e83d4d965785ce
reus <reusee@gmail.com>
2017-01-04 20:39:33 +0800

    move `readOnly` function to right place

--- mutable_state.js
+++ mutable_state.js
@@ -205,15 +205,3 @@ export class MutableState extends State {
   }
 }
 
-export function readOnly(obj) {
-  if (typeof obj !== 'object' || obj === null) {
-    return obj;
-  }
-  Object.defineProperty(obj, '__aff_read_only', {
-    configurable: false,
-    enumerable: false,
-    writable: false,
-    value: true,
-  });
-  return obj;
-}
--- state.js
+++ state.js
@@ -4,3 +4,16 @@ export class State {
   beforePatch() {}
   argsChanged() {}
 }
+
+export function readOnly(obj) {
+  if (typeof obj !== 'object' || obj === null) {
+    return obj;
+  }
+  Object.defineProperty(obj, '__aff_read_only', {
+    configurable: false,
+    enumerable: false,
+    writable: false,
+    value: true,
+  });
+  return obj;
+}
