8e946c4910e2ad07ce3ae450ce3cfcf120b8f6e1
reus <reusee@gmail.com>
2017-01-16 15:51:17 +0800

    update todomvc example

--- examples/todomvc/main.js
+++ examples/todomvc/main.js
@@ -1,8 +1,8 @@
 import {
-  App, t, on, css, $,
+  App, t, on, css, $, updater,
   section, header, footer, h1, p, a, div, span,
   input, ul, li, none, button, strong, label, checkbox,
-  $any, $push, $splice, $filter,
+  $any, $push, $splice, $filter, $merge,
   DebugPanel,
 } from '../../index'
 
@@ -11,6 +11,23 @@ const saved = JSON.parse(window.localStorage.getItem('todomvc'));
 const init_state = {
   todos: saved.todos || [],
   filter: saved.filter || 'All',
+
+  Header: {
+    updateTodos: updater('todos'),
+  },
+
+  TodoList: {
+    $ref: ['todos', 'filter'],
+
+    Todo: {
+      updateTodos: updater('todos'),
+    },
+  },
+
+  Footer: {
+    $ref: ['todos', 'filter'],
+    updateTodos: updater('todos'),
+  },
 };
 
 const app = new App(
@@ -24,7 +41,7 @@ const app = new App(
   init_state,
 );
 
-const Header = header($`.header`,
+const Header = (state) => header($`.header`,
   h1('todos'),
   input($`.new-todo`, 
     {
@@ -35,7 +52,7 @@ const Header = header($`.header`,
       if (e.keyCode != 13 || this.element.value.length == 0) {
         return
       }
-      app.update('todos', $push({
+      state.updateTodos($push({
         completed: false,
         content: this.element.value,
       }));
@@ -44,7 +61,7 @@ const Header = header($`.header`,
   ),
 );
 
-const Todo = (todo, i) => li(
+const Todo = (state, todo, i) => li(
   { 
     classList: {
       completed: todo.completed,
@@ -54,17 +71,17 @@ const Todo = (todo, i) => li(
   div($`.view`,
     checkbox($`.toggle`, 
       {
-        checked: todo.completed ? 'checked' : false,
+        checked: todo.completed,
       },
       on('click', function() {
-        app.update('todos', i, 'completed', this.element.checked);
+        state.updateTodos(i, 'completed', this.element.checked);
       }),
     ),
     label(todo.content, on('dblclick', () => {
-      app.update('todos', i, 'editing', true);
+      state.updateTodos(i, 'editing', true);
     })),
     button($`.destroy`, on('click', () => {
-      app.update('todos', $splice(i, 1));
+      state.updateTodos($splice(i, 1));
     })),
   ),
   input($`.edit`, 
@@ -73,29 +90,29 @@ const Todo = (todo, i) => li(
     },
     on('keypress', function(e) {
       if (e.keyCode == 13) {
-        app.updateMulti(
-          ['todos', i, 'content', this.element.value],
-          ['todos', i, 'editing', false],
-        );
+        state.updateTodos(i, $merge({
+          content: this.element.value,
+          editing: false,
+        }));
       }
     }),
   ),
 );
 
-const TodoList = (todos, filter) => ul($`.todo-list`, 
-  todos.map((todo, i) => {
-    if (filter == 'Active' && todo.completed) {
+const TodoList = (state) => ul($`.todo-list`, 
+  state.todos.map((todo, i) => {
+    if (state.filter == 'Active' && todo.completed) {
       return none;
-    } else if (filter == 'Completed' && !todo.completed) {
+    } else if (state.filter == 'Completed' && !todo.completed) {
       return none;
     }
-    return t(Todo, todo, i);
+    return t(Todo, state.Todo, todo, i);
   }),
 );
 
-const Footer = (todos, filter) => footer($`.footer`,
+const Footer = (state) => footer($`.footer`,
   span($`.todo-count`,
-    strong(todos.reduce((n, c) => {
+    strong(state.todos.reduce((n, c) => {
       return n + (c.completed ? 0 : 1);
     }, 0)),
     ' item left',
@@ -107,15 +124,15 @@ const Footer = (todos, filter) => footer($`.footer`,
   ].map(info => li(
     a(info[1], {
       style: `cursor: pointer`,
-      classList: filter == info[1] ? 'selected' : '', 
+      classList: state.filter == info[1] ? 'selected' : '', 
     }, on('click', () => { 
       window.location.hash = info[0];
     })),
   ))),
-  todos.reduce((b, c) => b || c.completed, false) ? button({
+  state.todos.reduce((b, c) => b || c.completed, false) ? button({
     classList: 'clear-completed',
     onclick() {
-      app.update('todos', $filter(todo => !todo.completed));
+      state.updateTodos($filter(todo => !todo.completed));
     },
   }, 'Clear completed') : none,
 );
@@ -129,18 +146,18 @@ const Info = footer($`.info`,
 
 const Main = (state) => div(
   section($`.todoapp`,
-    Header,
+    t(Header, state.Header),
     state.todos.length > 0 ? section($`.main`,
       checkbox($`.toggle-all`, on('click', () => {
-        let all_completed = app.state.todos.reduce((b, c) => b && c.completed, true);
-        app.update('todos', $any, 'completed', all_completed ? false : true);
+        let all_completed = state.todos.reduce((b, c) => b && c.completed, true);
+        state.$update('todos', $any, 'completed', all_completed ? false : true);
       })),
       label({
         for: 'toggle-all',
       }, 'Mark all as complete'),
-      t(TodoList, state.todos, state.filter),
+      t(TodoList, state.TodoList),
     ) : none,
-    state.todos.length > 0 ? t(Footer, state.todos, state.filter) : none,
+    state.todos.length > 0 ? t(Footer, state.Footer) : none,
   ),
   Info,
   DebugPanel(app),
