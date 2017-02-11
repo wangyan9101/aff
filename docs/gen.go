ded0df178a8790aea5868afb77fdd9cfe5029d49
reus <reusee@gmail.com>
2017-02-11 08:01:37 +0800

    simplify gen.go

--- docs/gen.go
+++ docs/gen.go
@@ -36,54 +36,8 @@ func main() {
 	ce(err, "output")
 }
 
-type Err struct {
-	Pkg  string
-	Info string
-	Prev error
-}
-
-func (e *Err) Error() string {
-	if e.Prev == nil {
-		return fmt.Sprintf("%s: %s", e.Pkg, e.Info)
-	}
-	return fmt.Sprintf("%s: %s\n%v", e.Pkg, e.Info, e.Prev)
-}
-
-func me(err error, format string, args ...interface{}) *Err {
-	if len(args) > 0 {
-		return &Err{
-			Pkg:  `affdocs`,
-			Info: fmt.Sprintf(format, args...),
-			Prev: err,
-		}
-	}
-	return &Err{
-		Pkg:  `affdocs`,
-		Info: format,
-		Prev: err,
-	}
-}
-
-func ce(err error, format string, args ...interface{}) {
+func ce(err error, args ...interface{}) {
 	if err != nil {
-		panic(me(err, format, args...))
+		panic(fmt.Errorf("%v %v", err, args))
 	}
-}
-
-func ct(err *error) {
-	if p := recover(); p != nil {
-		if e, ok := p.(error); ok {
-			*err = e
-		} else {
-			panic(p)
-		}
-	}
-}
-
-func oe(e error) error {
-	var ret error = e
-	for err, ok := ret.(*Err); ok && err.Prev != nil; err, ok = ret.(*Err) {
-		ret = err.Prev
-	}
-	return ret
 }
