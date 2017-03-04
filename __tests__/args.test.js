6deaf0f050f9bc0146d3d14f5a84d0c6a561b801
reus <reusee@gmail.com>
2017-03-04 22:33:52 +0800

    more test

--- __tests__/args.test.js
+++ __tests__/args.test.js
@@ -1,4 +1,4 @@
-import { MutableState } from '../index'
+import { MutableState, App, h, t, wo, op } from '../index'
 
 test('args change', () => {
   let state = new MutableState();
@@ -11,4 +11,42 @@ test('args change', () => {
   expect(state.argsChanged(1, '42')).toBe(true);
   let f = () => {}
   expect(state.argsChanged(f, () => {})).toBe(true);
+});
+
+test('function arg', () => {
+  const root = document.createElement('div');
+  const element = document.createElement('div');
+  root.appendChild(element);
+  let n = 0;
+  const app = new App(
+      element,
+      {
+        dict: {
+          foo: 'FOO',
+          bar: 'BAR',
+          baz: 'BAZ',
+        },
+        ids: ['foo', 'bar', 'baz'],
+        Foo: {
+          dict: wo('dict'),
+          fn: () => {},
+        },
+      },
+      (state) => h.div(
+        state.ids.map(id => t('Foo', (state, id) => {
+          n++;
+          const v = state.dict[id];
+          return h.p(v);
+        }, state.Foo, id)),
+      ),
+  );
+  expect(n).toBe(3);
+  app.update();
+  expect(n).toBe(3);
+  expect(root.innerHTML).toBe('<div><p>FOO</p><p>BAR</p><p>BAZ</p></div>');
+  app.state.ids.$update(op.shift);
+  expect(root.innerHTML).toBe('<div><p>BAR</p><p>BAZ</p></div>');
+  expect(n).toBe(5);
+  app.state.ids.$update(0, 'foo');
+  expect(n).toBe(6);
 });
